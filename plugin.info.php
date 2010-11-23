cef851ffbc9fe4083cd5d0e0ac0e08a3ceb30b66
Andre (Server) <webmaster@copamundial.eu>
2010-11-23 12:43:06 +0100

    renamed to plugin.info.txt

--- plugin.info.php
+++
@@ -1,7 +0,0 @@
-base schulzevote
-author Dominik Eckelmann
-email dokuwiki@cosmocode.de
-date 2010-05-06
-name Voting with Schulze Method
-desc a voting system per page using the schulze method
-url http://www.dokuwiki.org/plugin:schulzevote
