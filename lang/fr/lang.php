e95da7f2d81e5752be17fe685c66ecd4c383de9a
Laurent Forthomme <lforthomme@protonmail.com>
2021-10-30 17:54:25 +0200

    Added French

---
+++ lang/fr/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['no_remote_user'] = 'Veuillez vous connecter pour voter.';
+$lang['already_voted']  = 'Vous avez déjà voté.';
+$lang['vote_over']      = 'Ce vote est terminé.';
+$lang['cast']           = 'Voter';
+$lang['howto']          = 'Veuillez indiquer votre préférence par un nombre, le plus petit nombre représentant votre choix préféré.<br /><b>Attention</b> : vous ne pouvez voter qu\'une seule fois !';
+
+$lang['leading'] = '';
+//$lang['leading'] = '"%s" mène actuellement le vote.';
+$lang['has_won'] = '"%s" remporte le vote.';
+
+$lang['invalid_vote'] = 'Votre vote pour "%s" n\'est pas un nombre.';
+$lang['invalidated_vote'] = 'Votre vote n\'est pas valide. Veuillez vérifier que<ol><li>vous n\'avez pas déjà voté,</li><li>tous les scores sont différents, et</li><li>vous n\'avez pas omis un \'0\'.</li></ol>';
+$lang['vote'] = 'Voter';
+$lang['voted'] = 'Votre vote a bien été enregistré.';
+$lang['vote_cancel'] = 'Annuler votre vote';
